1a4c84da5672ed363e56ad6a7512c60dbae69624
Sylnit <h1samuel@yahoo.com>
2025-07-13 17:39:09 +0100

    Initial commit

---
+++ src/main/java/com/sylnit/ai_backend_demo/AiBackendDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sylnit.ai_backend_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBackendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBackendDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sylnit/ai_backend_demo/AiBackendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylnit.ai_backend_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBackendDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
