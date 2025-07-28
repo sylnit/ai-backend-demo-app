cbb5d21098cec69c93b483147029d8a6a29b84dd
Sylnit <h1samuel@yahoo.com>
2025-07-28 12:52:04 +0100

    Add tools to chat prompt

--- src/main/java/com/sylnit/ai_backend_demo/AiController.java
+++ src/main/java/com/sylnit/ai_backend_demo/AiController.java
@@ -1,5 +1,7 @@
 package com.sylnit.ai_backend_demo;
 
+import com.sylnit.ai_backend_demo.tools.CustomerTools;
+import com.sylnit.ai_backend_demo.tools.TransactionTools;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.http.ResponseEntity;
@@ -7,28 +9,39 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1")
 public class AiController {
 
-    private OllamaChatModel ollamaChatModel;
+    private final OllamaChatModel ollamaChatModel;
+    private final ChatClient chatClient;
+    private final TransactionTools transactionTools;
+    private final CustomerTools customerTools;
 
-    public AiController(OllamaChatModel ollamaChatModel) {
+    public AiController(OllamaChatModel ollamaChatModel, ChatClient chatClient, TransactionTools transactionTools, CustomerTools customerTools) {
         this.ollamaChatModel = ollamaChatModel;
+        this.chatClient = chatClient;
+        this.transactionTools = transactionTools;
+        this.customerTools = customerTools;
     }
 
     @GetMapping("/{message}")
     public ResponseEntity<?> getAnswer(
             @PathVariable(name = "message") String message
     ){
-        String response = ollamaChatModel.call(message);
+        //String response = ollamaChatModel.call(message); //use this model or use the chatClient
+        String response = chatClient.prompt()
+                .user(message)
+                .tools(transactionTools, customerTools)
+                .call()
+                .content();
         String cleaned = response.replaceAll("(?s)<think>.*?</think>", "").trim();
         System.out.println("Response: "+cleaned);
         Map<Object, Object> dataMap = new HashMap<>();
         dataMap.put("response", Util.stripMarkdown(cleaned));
         return ResponseEntity.ok(dataMap);
+
     }
 }
